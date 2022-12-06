d0790a2bf5a47c3198d32aa36d5a8a107ce9269d
B.OKELLO <bokello2010@gmail.com>
2022-12-06 07:16:51 +0300

    Entities
    Setting up of Entities

---
+++ src/main/java/com/mmmshop/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,30 @@
+package com.MMMshop.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="product_category")
+//@Data
+@Getter
+@Setter
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+
+
+}
