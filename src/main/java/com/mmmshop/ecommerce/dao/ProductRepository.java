84964401e1c18461544e3c2280e4a187b6db57c6
B.OKELLO <bokello2010@gmail.com>
2023-01-22 07:12:30 +0300

    Find by name - query method API

--- src/main/java/com/mmmshop/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/mmmshop/ecommerce/dao/ProductRepository.java
@@ -14,6 +14,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
       Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
-
+      Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
 
 }
